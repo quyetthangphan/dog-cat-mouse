ead2ab2946a570fa59c885e17018ca2fcec8af39
UniKey <pqt.lhu@gmail,com>
2021-03-28 13:30:33 +0700

    Dog class

--- Cat.js
+++ Cat.js
@@ -1,7 +1,9 @@
-function Cat(){
+fuucntion Cat(){
 	this.stomach = [];
 }
-Cat.prototype.eat = function(mouse){
+
+Cat.prototype.eat = fuucntion(){
 	this.stomach.push(mouse);
+
 };
 module.exports = Cat;
--- Mouse.js
+++ Mouse.js
@@ -1,4 +1,10 @@
 function Mouse(name) {
 	this.name = name;
+	this.dead = false;
 }
-module.exports = Mouse;
+
+Mouse.prototype.die = function(){
+	this.dead = true;
+}
+
+module.exports = Mouse; 
